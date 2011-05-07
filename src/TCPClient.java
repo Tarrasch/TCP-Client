87e2a415637395d714bba02804dc33cbe7f02c07
Arash Rouhani <miffoljud@gmail.com>
2011-05-07 14:23:49 +0200

    Removed some trash comments and included some functionality

---
+++ src/TCPClient.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.*;
+
+class TCPClient
+   {
+    public static final String GET_REQUEST = "GET / HTTP/1.0\n\n";
+
+   public static void main(String argv[]) throws Exception
+      {
+      String sentence;  /* sentence typed in by the user       */
+      String modifiedSentence; /* user's sentence after being  */
+                               /* modified by the server       */
+
+      BufferedReader inFromUser =
+         new BufferedReader(
+           new InputStreamReader(System.in));
+
+      String hostName = inFromUser.readLine();
+      int port = Integer.parseInt(inFromUser.readLine());
+
+      Socket clientSocket = new Socket(hostName, port);
+
+      DataOutputStream outToServer =
+         new DataOutputStream(
+           clientSocket.getOutputStream());
+      BufferedReader inFromServer =
+         new BufferedReader(new InputStreamReader(
+           clientSocket.getInputStream()));
+
+      /* Send the user's inputted line to the server */
+      outToServer.writeBytes(GET_REQUEST);
+
+      /* Get the sentence back from the server after being modified */
+      modifiedSentence = inFromServer.readLine();
+
+      /* Output the modified sentence */
+      System.out.println("FROM TCP SERVER: " +
+         modifiedSentence);
+
+      /* close the TCP socket */
+      clientSocket.close();
+      }
+   }
